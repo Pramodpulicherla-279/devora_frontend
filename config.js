a32c4ad4fbfb8d8801ae41b68c8e098e8c395180
Pramodpulicherla-279 <pramodpulicherla279@gmail.com>
2026-05-31 23:17:34 +0530

    added forgot password functionality

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // const API_BASE_URL = "http://localhost:5000";
 // const API_BASE_URL = "http://192.168.0.123:5000";
 const API_BASE_URL = "https://devora-backend.onrender.com";
-
 export { API_BASE_URL };
